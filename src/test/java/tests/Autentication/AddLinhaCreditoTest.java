91836e5b15cfe11c6e1786d9c3ed026f0e6dc9cf
Fellipe Oliveira de Lima <oliveirafellipe@live.com>
2018-03-07 14:50:29 -0300

    Initial commit

---
+++ src/test/java/tests/Autentication/AddLinhaCreditoTest.java
@@ -0,0 +1,22 @@
+package tests.Autentication;
+
+import Helpers.BaseComLogin;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import static org.junit.Assert.assertEquals;
+
+//Extenda sua classe do BaseComLogin para não precisar instanciar sempre um novo browser
+public class AddLinhaCreditoTest extends BaseComLogin {
+
+    @Test
+    public void addLinhaCredito()  {
+        //Seu script de teste automatizado vai aqui
+
+    }
+    @Test
+    public void addLinhaCredito2(){
+        //Seu script de teste automatizado vai aqui
+}
